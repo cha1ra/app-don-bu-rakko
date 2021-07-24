a4cbd92c72d3572cf83f8193d48245bd3788bfd1
cha1ra <cha1ra.garnet@gmail.com>
2021-07-25 01:29:52 +0900

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  reactive
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {
+    const flag = ref()
+    const state = reactive({
+
+    })
+    return {
+      flag,
+      state
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+
+</style>
